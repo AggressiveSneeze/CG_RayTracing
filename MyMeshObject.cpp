1b87b9b92ee025dc856c917886304a4a15d731b5
James Adams <adj@student.unimelb.edu.au>
2016-01-13 13:11:07 +0200

    bounding sphere calculation done but not tested

--- MyMeshObject.cpp
+++ MyMeshObject.cpp
@@ -3,13 +3,17 @@
 //
 
 #include "MyMeshObject.h"
-
+#include "MyMesh.h"
+#include "sphere.h"
 MyMeshObject::MyMeshObject(MyMesh & mesh): _mesh(mesh)) {
 
 	//maybe something here to compute bounding sphere and centre.
 	//is it really necessary? Can't just check if ray intersects with any of the triangles?
 
-};
+	//TODO: What's wrong with this call?
+	calculateBoundingSphere();
+
+}
 
 
 int intersect(IN Ray& ray, IN double tMax, OUT double& t, OUT Point3d& P, 
@@ -21,82 +25,79 @@ int intersect(IN Ray& ray, IN double tMax, OUT double& t, OUT Point3d& P,
 
 };
 
-void calculateBoundingSphere() {
+void MyMeshObject::calculateBoundingSphere() {
 
 	Vector3d center;
-	float radius;
-	Vector3d xmin,xmas,ymin,ymax,zmin,zmax;
-	xmin=ymin=zmin=Vector3d(1,1,1)*INF;
-	xmax=ymin=zmin=Vector3d(-1,-1,-1)*INF;
+	Vector3d xmin,xmax,ymin,ymax,zmin,zmax;
+	xmin=ymin=zmin=Vector3d(1.0,1.0,1.0)*INF;
+	xmax=ymin=zmin=Vector3d(-1.0,-1.0,-1.0)*INF;
 
-	for (MyMesh::VertexIter v_it=mesh.vertices_begin(); 
-					v_it!=mesh.vertices_end(); ++v_it) {
-		if (v_it.x<xmin.x) xmin=v_it;
-		if (v_it.x>xmax.x) xmax=v_it;
-		if (v_it.y<ymin.y) ymin=v_it;
-		if (v_it.y>ymax.y) ymax=v_it;
-		if (v_it.z<zmin.z) zmin=v_it;
-		if (v_it.z>zmax.z) zmax=v_it;
+	for (MyMesh::VertexIter v_it=_mesh.vertices_begin();
+					v_it!=_mesh.vertices_end(); ++v_it) {
+		if (v_it.x < xmin.x) xmin = _mesh.point(v_it);
+		if (v_it.x > xmax.x) xmax = _mesh.point(v_it);
+		if (v_it.y < ymin.y) ymin = _mesh.point(v_it);
+		if (v_it.y > ymax.y) ymax = _mesh.point(v_it);
+		if (v_it.z < zmin.z) zmin = _mesh.point(v_it);
+		if (v_it.z > zmax.z) zmax = _mesh.point(v_it);
 	}
-	
 
-	//  public struct BoundingSphere
- // {
- //     public Vector3 center;
- //     public float radius;
- //     public BoundingSphere(Vector3 aCenter, float aRadius)
- //     {
- //         center = aCenter;
- //         radius = aRadius;
- //     }
- 
- //     public static BoundingSphere Calculate(IEnumerable<Vector3> aPoints)
- //     {
- //         Vector3 xmin, xmax, ymin, ymax, zmin, zmax;
- //         xmin = ymin = zmin = Vector3.one * float.PositiveInfinity;
- //         xmax = ymax = zmax = Vector3.one * float.NegativeInfinity;
- //         foreach(var p in aPoints)
- //         {
- //             if(p.x < xmin.x) xmin = p;
- //             if(p.x > xmax.x) xmax = p;
- //             if(p.y < ymin.y) ymin = p;
- //             if(p.y > ymax.y) ymax = p;
- //             if(p.z < zmin.z) zmin = p;
- //             if(p.z > zmax.z) zmax = p;
- //         }
-			//magnitude squared. 
- //         var xSpan = (xmax - xmin).sqrMagnitude;
- //         var ySpan = (ymax - ymin).sqrMagnitude;
- //         var zSpan = (zmax - zmin).sqrMagnitude;
- //         var dia1 = xmin;
- //         var dia2 = xmax;
- //         var maxSpan = xSpan;
- //         if (ySpan > maxSpan)
- //         {
- //             maxSpan = ySpan;
- //             dia1 = ymin; dia2 = ymax;
- //         }
- //         if (zSpan > maxSpan)
- //         {
- //             dia1 = zmin; dia2 = zmax;
- //         }
- //         var center = (dia1 + dia2) * 0.5f;
- //         var sqRad = (dia2 - center).sqrMagnitude;
- //         var radius = Mathf.Sqrt(sqRad);
- //         foreach (var p in aPoints)
- //         {
- //             float d = (p - center).sqrMagnitude;
- //             if(d > sqRad)
- //             {
- //                 var r = Mathf.Sqrt(d);
- //                 radius = (radius + r) * 0.5f;
- //                 sqRad = radius * radius;
- //                 var offset = r - radius;
- //                 center = (radius * center + offset * p) / r;
- //             }
- //         }
- //         return new BoundingSphere(center, radius);
- //     }
- // }
+	float x_span=(xmax-xmin).sqrnorm();
+	float y_span=(ymax-ymin).sqrnorm();
+	float z_span=(zmax-zmin).sqrnorm();
+	Vector3d dia1 = xmin;
+	Vector3d dia2 = xmax;
+	float max_span=x_span;
+	if (y_span > max_span)
+	{
+		max_span = y_span;
+		dia1 = ymin; dia2 = ymax;
+	}
+	if (z_span > max_span)
+	{
+		dia1 = zmin; dia2 = zmax;
+	}
+	center = (dia1 + dia2) * 0.5f;
+
+	float sqRad = (dia2 - center).sqrnorm();
+	float radius = sqrtf(sqRad);
+
+	float d,r,offset;
+	for (MyMesh::VertexIter v_it=_mesh.vertices_begin();
+		 v_it!=_mesh.vertices_end(); ++v_it) {
+		d=(_mesh.point(v_it)-center).sqrnorm();
+		if(d>sqRad) {
+			r=sqrtf(d);
+			radius=(radius+r)*0.5f;
+			sqRad=radius*radius;
+			offset=r-radius;
+			//TODO:is this line okay using the defined operators?
+			center=(radius*center+offset*_mesh.point(v_it))/r;
+		}
+	}
+	//now we have center and radius.
+	//assign the bounding sphere
+	*_boundingSphere=Sphere(center,radius);
+	//Todo: can the centre just be:
+	_center=center;
+	//ported from
+	//http://answers.unity3d.com/questions/836915/smallest-bounding-sphere.html
+
+}
+
+
+int MyMeshObject::intersect(IN Ray& ray, IN double tMax, OUT double& t, OUT Point3d& P, OUT Vector3d& N, OUT Color3d& texColor) {
+
+	//check bounding sphere first. early exit if ray doesn't even go through bounding sphere.
+	if(!_boundingSphere->intersect(ray,tMax,t,P,N,texColor)) return 0;
+	//psuedo code:
+	//for every face
+	//check intersection between ray and triangle.
+	//have to go through every face in case there is a closer one.
+
+
+
+
+
 
 }
